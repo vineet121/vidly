7bcca1e405e28a1cdf94ad41cc4044cf19c16d48
vineet121 <vineetdahiya2000@gmail.com>
2021-11-05 15:47:03 +0530

    Added API's

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Movie
 
 def index(request):
-    return HttpResponse("Hello World")
+    movies = Movie.objects.all()
+    output=', '.join([m.title for m in movies])
+    return HttpResponse(output)
 
 # Create your views here.
