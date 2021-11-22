4296c9d164d55101a7f5129bdca15a7936b82a04
vineet121 <vineetdahiya2000@gmail.com>
2021-11-22 19:57:49 +0530

    common base template

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,7 @@
+from typing import NamedTuple
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='movies_index'),
+    path('<movie_id>', views.detail, name ='movies_detail')
 ]
--- movies/views.py
+++ movies/views.py
@@ -8,5 +8,6 @@ def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', { 'movies':movies})
 
-
+def detail(request, movie_id):
+    return HttpResponse(movie_id)
 # Create your views here.
